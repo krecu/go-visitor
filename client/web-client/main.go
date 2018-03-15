cc807986fea0c0beec1edf02df9be75a46d6a0fb
Kretsu Evgeniy <krecu.me@ya.ru>
2018-03-15 12:45:31 +0300

    fix web client interface

--- client/web-client/main.go
+++ client/web-client/main.go
@@ -1,11 +1,14 @@
 package web_client
 
 import (
-	"net/http"
-	"time"
 	"bytes"
 	"encoding/json"
 	"io/ioutil"
+	"net/http"
+	"time"
+
+	"fmt"
+
 	"github.com/krecu/go-visitor/model"
 )
 
@@ -19,17 +22,46 @@ type Body struct {
 	Id string
 }
 
-func New(addr string) (*Client) {
+type Patch struct {
+	Id     string
+	Fields string
+}
+
+func New(addr string) *Client {
 
 	return &Client{
 		Addr: addr,
 	}
 }
 
-func (v *Client) Get(id string, ip string, ua string) (visitor *model.Visitor, err error){
-	var proto model.Visitor
+func (v *Client) Get(id string) (proto *model.Raw, err error) {
 
-	url := v.Addr + "/api/visitor"
+	url := fmt.Sprintf("%s/api/visitor/%s", v.Addr, id)
+
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("Content-Type", "application/json")
+
+	var client = &http.Client{
+		Timeout: time.Second * 1,
+	}
+	response, err := client.Do(req)
+
+	if err != nil || response.StatusCode != 200 {
+		return &model.Raw{}, err
+	}
+	defer response.Body.Close()
+
+	result, _ := ioutil.ReadAll(response.Body)
+
+	err = json.Unmarshal(result, &proto)
+
+	return
+}
+
+// создание на GRPC
+func (v *Client) Post(id string, ip string, ua string, extra map[string]interface{}) (proto *model.Raw, err error) {
+
+	url := fmt.Sprintf("%s/api/visitor", v.Addr)
 
 	body, err := json.Marshal(&Body{
 		Ip: ip,
@@ -38,19 +70,19 @@ func (v *Client) Get(id string, ip string, ua string) (visitor *model.Visitor, e
 	})
 
 	if err != nil {
-		return &model.Visitor{}, err
+		return
 	}
 
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(body))
 	req.Header.Set("Content-Type", "application/json")
 
 	var client = &http.Client{
-		Timeout: time.Second * 100,
+		Timeout: time.Second * 1,
 	}
 	response, err := client.Do(req)
 
 	if err != nil || response.StatusCode != 200 {
-		return &model.Visitor{}, err
+		return
 	}
 	defer response.Body.Close()
 
@@ -58,5 +90,48 @@ func (v *Client) Get(id string, ip string, ua string) (visitor *model.Visitor, e
 
 	err = json.Unmarshal(result, &proto)
 
-	return &proto, err
+	return
+}
+
+// создание на GRPC
+func (v *Client) Patch(id string, fields map[string]interface{}) (proto *model.Raw, err error) {
+
+	var (
+		filedJson []byte
+	)
+
+	filedJson, err = json.Marshal(fields)
+	if err != nil {
+		return
+	}
+
+	url := fmt.Sprintf("%s/api/visitor/%s", v.Addr, id)
+
+	body, err := json.Marshal(&Patch{
+		Id:     id,
+		Fields: string(filedJson),
+	})
+
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest("PATCH", url, bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	var client = &http.Client{
+		Timeout: time.Second * 1,
+	}
+	response, err := client.Do(req)
+
+	if err != nil || response.StatusCode != 200 {
+		return
+	}
+	defer response.Body.Close()
+
+	result, _ := ioutil.ReadAll(response.Body)
+
+	err = json.Unmarshal(result, &proto)
+
+	return
 }
